0dade031ff67c70c77b8e1f429302321be4c9113
mnishikizawa <mnishikizawa@gmail.com>
2017-01-26 12:54:36 +0900

    Update ntp_spec.rb

--- ntp_spec.rb
+++ ntp_spec.rb
@@ -7,7 +7,7 @@ describe "Check time syncronization" do
   end
 
   it "offset should be small" do
-     offset = command("ntpq -p |grep '^\*' |awk '{print $8}'").stdout.to_i
+     offset = command("ntpq -p |grep '^\*' |awk '{print $9}'").stdout.to_i
      expect(offset).to be_within(100).of(0)
   end
 end
